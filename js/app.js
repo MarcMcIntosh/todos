c6fef822fb579a392230db6496e66a27a8668af3
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-16 12:43:47 +0200

    Moved header view component to file and imported

--- js/app.js
+++ js/app.js
@@ -2,25 +2,7 @@
 * add imports javascript here
 */
 // https://www.contentful.com/blog/2017/04/04/es6-modules-support-lands-in-browsers-is-it-time-to-rethink-bundling/
-function header() {
-  const element = document.createElement('header');
-  element.className = 'header';
-
-  const h1 = document.createElement('h1');
-  const text = document.createTextNode('todos');
-  h1.appendChild(text);
-
-  const input = document.createElement('input');
-  input.className = "new-todo";
-  input.setAttribute('placeholder', "What needs to be done?");
-  input.setAttribute('autofocus', true);
-
-
-  element.appendChild(h1);
-  element.appendChild(input);
-
-  return element;
-}
+import { header } from './views';
 
 function app() {
   const element = document.createElement('section');
