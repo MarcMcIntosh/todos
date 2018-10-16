5fdfb599431205d8c4a7a8b41a91dfed8fae8a87
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-16 12:49:37 +0200

    Trying to fix issues with resloving files.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 * add imports javascript here
 */
 // https://www.contentful.com/blog/2017/04/04/es6-modules-support-lands-in-browsers-is-it-time-to-rethink-bundling/
-import { header } from './views';
+import { header } from './views/index.js';
 
 function app() {
   const element = document.createElement('section');
--- js/views/index.js
+++ js/views/index.js
@@ -3,4 +3,4 @@
 // https://www.w3schools.com/jsref/dom_obj_all.asp
 // https://www.contentful.com/blog/2017/04/04/es6-modules-support-lands-in-browsers-is-it-time-to-rethink-bundling/
 
-export { default as header } from './header';
+export { default as header } from './header.js';
