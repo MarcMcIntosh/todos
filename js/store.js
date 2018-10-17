59861cd804cb205c24f27696dd51dfa5bf10ae21
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-17 11:36:03 +0200

    Added store functionality

---
+++ js/store.js
@@ -0,0 +1,55 @@
+/*
+craete item
+read all items
+update an item
+delete an item
+*/
+
+const { localStorage } = window;
+
+function setItem({
+  id = ''
+  completed = false,
+  created_at = 0,
+  label,
+} = {}) {
+  const update_at = new Date();
+
+  const obj = {
+    id: id || `todos-marc-${created_at || updated_at}`,
+    completed,
+    label,
+    created_at: created_at || update_at,
+    update_at,
+  };
+  return localStorage.setItem(id, JSON.stringify(obj));
+}
+
+function getItem(id) {
+  const itemString = localStorage.getItem(id);
+  return JSON.parse(itemString);
+}
+
+function createItem(label) {
+  return setItem({ label });
+}
+
+function updateItem(item) {
+  const oldItem = getItem(item.id);
+  return setItem({ ...oldItem, ...item });
+}
+
+function getAll() {
+  const todos = [];
+  for (let i = (localStorage.length - 1); i > 0; i =-1) {
+    todos[i] = localStorage.key(i);
+  }
+  return todos.reduce((a, b) => Object.assign({}, a, { [b.id] : b }), {});
+}
+
+function removedItem({ id }) {
+  localStorage.removedItem(id);
+  return getAll();
+}
+
+export { getAll, updateItem, createItem, removedItem };
