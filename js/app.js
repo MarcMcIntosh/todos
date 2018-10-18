9f8d47de453751f5176532f1ad664859381e2d1b
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-18 12:50:12 +0200

    App now uses store.getAll, to decide what to display

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 */
 // https://www.contentful.com/blog/2017/04/04/es6-modules-support-lands-in-browsers-is-it-time-to-rethink-bundling/
 import { header, footer, todoList } from './views/index.js';
-
+import { getAll } from './store/js';
 function app() {
 
   const element = document.createElement('section');
@@ -11,7 +11,7 @@ function app() {
   element.appendChild(header());
 
   // footer
-  if (window.localStorage.length) {
+  if (getAll().length) {
     element.innerHTML = todoList();
     element.appendChild(footer());
 
