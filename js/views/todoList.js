de78fdddcdfa408e3e15a975d72f25e79ae6e25b
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-18 12:06:40 +0200

    Added the main todo list

--- js/views/todoList.js
+++ js/views/todoList.js
@@ -5,3 +5,17 @@
                   //        <label for="toggle-all">Mark all as complete</label>
 //	<ul class="todo-list">[ITEMS]</ul>
 // </section>
+import { getAll } from '../store.js';
+import todoItem from './todoItem.js';
+
+export default function todoList() {
+  const todos = Object.values(getAll());
+  const listItems = todos.map(todoItem);
+  return `
+    <section class="main">
+      <input id="toddle-all" class="toggle-call" type="checkbox" />
+      <label for="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">${listItems.join('\n')}</ul>
+    </section>
+  `;
+}
