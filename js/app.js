f699332ff0cfbbfcd82b2223f407f401f7adb03d
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-18 12:58:27 +0200

    Typoe importing store

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 */
 // https://www.contentful.com/blog/2017/04/04/es6-modules-support-lands-in-browsers-is-it-time-to-rethink-bundling/
 import { header, footer, todoList } from './views/index.js';
-import { getAll } from './store/js';
+import { getAll } from './store.js';
 function app() {
 
   const element = document.createElement('section');
