1b09bfc0987a81a069e33a7d51b7fa9a2895dacd
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-18 12:06:05 +0200

    Added todo list item view

--- js/views/todoItem.js
+++ js/views/todoItem.js
@@ -7,3 +7,23 @@
   </div>
    <input class="edit" value="Create a TodoMVC template">
 </li> */
+
+export default function listItem({
+  completed,
+  // editting,
+  title,
+  // onEdit,
+  // onChange,
+}) {
+  const cn = completed ? "completed" : editting ? 'editting': "";
+  return `
+    <li class=${cn}>
+      <div class="view">
+        <input class="toggle" type="checkbox" checked />
+        <label>${title}</label>
+        <button class="destroy" />
+      </div>
+      <input class="edit" value="Create a TodoMVC template" />
+    </li>`;
+
+}
