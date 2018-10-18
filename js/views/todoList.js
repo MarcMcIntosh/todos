0f74848105e067018c581e84a625da830c385bf5
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-18 14:41:54 +0200

    Removed logging

--- js/views/todoList.js
+++ js/views/todoList.js
@@ -21,9 +21,6 @@ export default function todoList() {
   root.className = 'main';
   appendList(root);
   /* automatic redraw on list changes */
-  window.addEventListener('storage', function(event) {
-    console.log(event);
-    appendList(root);
-  });
+  window.addEventListener('storage', function(event) { appendList(root); });
   return root;
 }
