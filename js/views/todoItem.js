5cbee8c5a5f2a5fb16061d296a471e14f8c7a351
Marc McIntosh <Marc.McIntosh@holidayCheck.com>
2018-10-19 12:39:22 +0200

    change id to string

--- js/views/todoItem.js
+++ js/views/todoItem.js
@@ -30,7 +30,7 @@ export default function listItem({
     <li class=${cn}>
       <div class="view">
         <label>${title}</label>
-        <button id=${id} class="destroy" onclick="removeItem(${id})"/>
+        <button id=${id} class="destroy" onclick="removeItem('${id}')"/>
       </div>
       <input class="edit" value="Create a TodoMVC template" />
     </li>`;
